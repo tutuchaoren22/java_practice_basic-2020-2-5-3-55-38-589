cd36cebc5d142c0369ab6548377d3cd2deb1a469
duyuqian <15738381799@163.com>
2020-02-09 12:21:23 +0800

    [duyuqian]: create object use Student class and methods

--- 002_object/src/main/java/Application.java
+++ 002_object/src/main/java/Application.java
@@ -1,5 +1,14 @@
+import model.Student;
+
 public class Application {
 
     public static void main(String[] args) {
+        Student stuLily = new Student("李莉",20,"女");
+        System.out.println( stuLily.getName() );
+        System.out.println( stuLily.getAge());
+        System.out.println( stuLily.getGender());
+        stuLily.study();
+        stuLily.rest();
+
     }
 }
