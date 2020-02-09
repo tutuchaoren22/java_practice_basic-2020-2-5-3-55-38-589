e995d7481616ca23742279ce4e44cc2d17f218c1
duyuqian <15738381799@163.com>
2020-02-09 12:30:18 +0800

    [duyuqian]: create object use Teacher class and print all students he teached

--- 002_object/src/main/java/Application.java
+++ 002_object/src/main/java/Application.java
@@ -1,4 +1,7 @@
 import model.Student;
+import model.Teacher;
+
+import java.util.ArrayList;
 
 public class Application {
 
@@ -10,5 +13,15 @@ public class Application {
         stuLily.study();
         stuLily.rest();
 
+        Student stuWangQi = new Student("王其",21,"男");
+        ArrayList<Student> students = new ArrayList<Student>();
+        students.add(stuLily);
+        students.add(stuWangQi);
+        Teacher teacher=new Teacher("张龙",students);
+        Student stuZhaoYi = new Student("赵毅",22,"男");
+        teacher.addStudent(stuZhaoYi);
+        for (Student student:teacher.getStudentList()) {
+            System.out.println("学生姓名："+student.getName()+"，年龄："+student.getAge()+"，性别："+student.getGender());
+        }
     }
 }
